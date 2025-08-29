b413f2853d3b6409d7e3805b9b52e2b83f72cbdf
Vinod  Gupta <vinod.gupta@finicity.com>
2025-08-29 19:04:44 +0530

    add workflow

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -35,7 +35,7 @@ const Chat = ({ location }) => {
         alert(error);
       }
     });
-  }, [ENDPOINT, location.search]);
+  }, [location.search]);
 
   useEffect(() => {
     socket.on("message", (message) => {
